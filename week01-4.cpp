3fc1b3a6d1014024946dad8f2822ea782bc42481
13750652 <yen22011109@gmail.com>
2025-09-10 12:03:04 +0800

    WEEK01

---
+++ week01-4.cpp
@@ -0,0 +1,13 @@
+///week01-4b.cpp ¨Ï¥ÎC»y¨¥
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    int a,b;
+	cin >> a >> b;
+	int ans = 0;
+	for(int i=a;i<=b;i++){
+		if(i%3==0);
+	}
+	cout << ans;
+}
